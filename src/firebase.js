eb36fceecd48990b5b205d2f583abbfcc201040e
Anvit Deshpande <70889070+Anvit3@users.noreply.github.com>
2020-12-29 14:39:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcN04Bi-7fiQKELmRSGZ8Mk4bGLj40llo",
-  authDomain: "telegram-clone-5d7af.firebaseapp.com",
-  databaseURL: "https://telegram-clone-5d7af-default-rtdb.firebaseio.com",
-  projectId: "telegram-clone-5d7af",
-  storageBucket: "telegram-clone-5d7af.appspot.com",
-  messagingSenderId: "1085217017076",
-  appId: "1:1085217017076:web:c193025a9b03b38d1862a1",
-  measurementId: "G-MPTHM2P42Y"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABSE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
